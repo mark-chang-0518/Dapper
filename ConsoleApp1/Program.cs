6fd16ba54b4060b5aff52892654fba9e8d86a138
mark <mark.ch.hu.chang@gmail.com>
2018-12-06 00:03:55 +0800

    update by version 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace ConsoleApp1
 {
@@ -34,16 +35,20 @@ namespace ConsoleApp1
                 #region 檢查有無DataTable(Users) 如無則新增DataTable(Users)
                 //檢查有無DataTable 如無則建立DataTable
                 CheckDataTable();
-                #endregion
+                #endregion               
 
                 #region ADO 查詢
-                //var ADO_data = 0;
+                SelectUserByAdoNet();
                 #endregion
 
                 #region Dapper 查詢
-                //Console.WriteLine(string.Empty);
-                //var Dapper_data = SelectUserByDapper();
-                //ShowData(Dapper_data);
+                //int[] searchDatas = null;
+                //searchDatas = new int[] { 1002, 1003, 1007 };
+                //SelectUserByDapper(searchDatas);
+                #endregion
+
+                #region 使用 Dynamic 查詢
+                //SelectUseDynamic();
                 #endregion
 
                 #region Dapper 新增
@@ -72,9 +77,7 @@ namespace ConsoleApp1
                 //        LastUpdateTime = dt,
                 //    }
                 //};
-                //int insert_Result = InsertUsers(datas);
-                //Console.WriteLine(string.Empty);
-                //Console.WriteLine(string.Format("新增資料{0}筆", insert_Result));
+                //InsertUsers(datas);
                 #endregion
 
                 #region Dapper 修改
@@ -98,9 +101,7 @@ namespace ConsoleApp1
                 //        LastUpdateTime = dt,
                 //    }
                 //};
-                //int update_Result = UpdateUsers(datas);
-                //Console.WriteLine(string.Empty);
-                //Console.WriteLine(string.Format("修改資料{0}筆", update_Result));
+                //UpdateUsers(datas);
                 #endregion
 
                 #region Dapper 刪除
@@ -113,28 +114,32 @@ namespace ConsoleApp1
                 //        UserId = 2,
                 //    }
                 //};
-                //int delete_Result = DeleteUsers(datas);
-                //Console.WriteLine(string.Empty);
-                //Console.WriteLine(string.Format("刪除資料{0}筆", delete_Result));
+                //DeleteUsers(datas);
                 #endregion
 
                 #region Dapper Transaction
-
+                //Transaction();
                 #endregion
 
                 #region Dapper TransactionScope
-
+                //TransactionScope();
                 #endregion
 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("錯誤:{0}", ex.Message));
+                //throw ex;
+            }
+            finally
+            {
                 Console.WriteLine(string.Empty);
                 Console.WriteLine("==========程式結束==========");
                 Console.ReadLine();
             }
-            catch (Exception ex)
-            {
-                throw ex;
-            }       
         }
+
+        #region 前置動作 & 預設
 
         /// <summary>
         /// 連線字串
@@ -151,7 +156,7 @@ namespace ConsoleApp1
             {
                 if (!context.Database.Exists())
                     ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
-            }           
+            }
         }
 
         /// <summary>
@@ -167,11 +172,15 @@ CREATE TABLE Users(UserId int IDENTITY(1,1) not null, Email nvarchar(max),Name n
 CONSTRAINT PK_Users PRIMARY KEY([UserId]))";
 
                 using (SqlCommand command = new SqlCommand(commandStr, conn))
-                {                    
+                {
                     command.ExecuteNonQuery();
                 }
             }
         }
+
+        #endregion
+
+        #region CRUD
 
         /// <summary>
         /// 查詢資料 By ADO.NET
@@ -179,25 +188,87 @@ CONSTRAINT PK_Users PRIMARY KEY([UserId]))";
         /// <returns></returns>
         private static IEnumerable<Users> SelectUserByAdoNet()
         {
-            return new List<Users>();
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("----------使用ADO.NET查詢----------");
+
+            List<Users> results = new List<Users>();
+
+            using (SqlConnection conn = new SqlConnection(DefaultConn))
+            {
+                conn.Open();
+                string strSql = @"
+select [UserId]
+      ,[Email]
+      ,[Name]
+      ,[IsEnable]
+      ,[GuidID]
+      ,[Money]
+      ,[LastUpdateTime] from Users ;";
+                using (SqlCommand com = new SqlCommand(strSql, conn))
+                {
+                    SqlDataReader reader = com.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Nullable<Guid> ng = null;
+                        Guid g = new Guid();
+
+                        Users user = new Users()
+                        {
+                            UserId = Convert.ToInt32(reader["UserId"]),
+                            Email = Convert.ToString(reader["Email"]),
+                            Name = Convert.ToString(reader["Name"]),
+                            IsEnable = Convert.ToBoolean(reader["IsEnable"]),
+                            //GuidID = (Guid.TryParse(reader["GuidID"]?.ToString(), out g)) ? g : ng,
+                            GuidID = (Guid.TryParse(((reader["GuidID"] == null) ? null : reader["GuidID"].ToString()), out g)) ? g : ng,
+                            Money = Convert.ToDecimal(reader["Money"]),
+                            LastUpdateTime = Convert.ToDateTime(reader["LastUpdateTime"]),
+                        };
+                        results.Add(user);
+                    }
+                    reader.Close();
+                }
+
+            }
+            if (results.Any())
+                ShowData(results);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Format("-----ADO.NET查詢資料數為{0}筆-----", results.Count));
+
+            return results;
         }
 
         /// <summary>
         /// 查詢資料 By Dapper
         /// </summary>
         /// <returns></returns>
-        private static IEnumerable<Users> SelectUserByDapper()
+        private static IEnumerable<Users> SelectUserByDapper(int[] Ids = null)
         {
-            Console.WriteLine("使用Dapper查詢");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("----------使用Dapper查詢----------");
 
             List<Users> results = new List<Users>();
             Users result = new Users();
             using (SqlConnection conn = new SqlConnection(DefaultConn))
             {
-                
-                string strSql = @"select * from Users ;";
+
+                string strSql = @"
+select [UserId]
+      ,[Email]
+      ,[Name]
+      ,[IsEnable]
+      ,[GuidID]
+      ,[Money]
+      ,[LastUpdateTime] from Users ";
+
+                if (Ids != null && Ids.Any())
+                {
+                    strSql += @" Where UserId in @UserIds ";
+                }
+
+                strSql += ";";
+
                 //查詢
-                results = conn.Query<Users>(strSql).ToList();
+                results = conn.Query<Users>(strSql, new { UserIds = Ids }).ToList();
 
                 //查詢第一筆 (不可以查無資料)
                 //result = conn.QueryFirst<Users>(strSql);
@@ -213,8 +284,47 @@ CONSTRAINT PK_Users PRIMARY KEY([UserId]))";
 
                 //results.Add(result);
             }
+            if (results.Any())
+                ShowData(results);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Format("-----Dapper查詢資料數為{0}筆-----", results.Count));
 
-            Console.WriteLine(string.Format("Dapper查詢資料數為{0}筆", results.Count));
+            return results;
+        }
+
+        /// <summary>
+        /// select use dynamic
+        /// </summary>
+        /// <returns></returns>
+        private static IEnumerable<dynamic> SelectUseDynamic()
+        {
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("----------查詢使用dynamic類別----------");
+            List<dynamic> results = new List<dynamic>();
+
+            using (SqlConnection conn = new SqlConnection(DefaultConn))
+            {
+                string strSql = @"
+select [UserId]
+      ,[Email]
+      ,[Name]
+      ,[IsEnable]
+      ,[GuidID]
+      ,[Money]
+      ,[LastUpdateTime] from Users ";
+
+                results = conn.Query<dynamic>(strSql).ToList();
+
+                foreach (var item in results)
+                {
+                    
+                    Console.WriteLine(string.Format("UserId:{0}", item.UserId));
+                    Console.WriteLine(string.Format("Email:{0}", item.Email ?? string.Empty));
+                }
+            }
+
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Format("-----查詢資料數為{0}筆-----", results.Count));
 
             return results;
         }
@@ -226,8 +336,10 @@ CONSTRAINT PK_Users PRIMARY KEY([UserId]))";
         /// <returns></returns>
         private static int InsertUsers(IEnumerable<Users> models)
         {
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("----------新增資料----------");
             int result = 0;
-       
+
             using (SqlConnection conn = new SqlConnection(DefaultConn))
             {
                 string strSql = @"
@@ -249,6 +361,9 @@ INSERT INTO [Users]
                 result = conn.Execute(strSql, models);
             }
 
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Format("-----新增資料{0}筆-----", result));
+
             return result;
         }
 
@@ -259,6 +374,9 @@ INSERT INTO [Users]
         /// <returns></returns>
         private static int UpdateUsers(IEnumerable<Users> models)
         {
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("----------修改資料----------");
+
             int result = 0;
 
             using (SqlConnection conn = new SqlConnection(DefaultConn))
@@ -276,6 +394,9 @@ INSERT INTO [Users]
                 result = conn.Execute(strSql, models);
             }
 
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Format("-----修改資料{0}筆-----", result));
+
             return result;
         }
 
@@ -286,6 +407,9 @@ INSERT INTO [Users]
         /// <returns></returns>
         private static int DeleteUsers(IEnumerable<Users> models)
         {
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("----------刪除資料----------");
+
             int result = 0;
 
             using (SqlConnection conn = new SqlConnection(DefaultConn))
@@ -296,6 +420,9 @@ INSERT INTO [Users]
 ";
                 result = conn.Execute(strSql, models);
             }
+
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Format("-----刪除資料{0}筆-----", result));
 
             return result;
         }
@@ -309,11 +436,124 @@ INSERT INTO [Users]
             foreach (var item in datas)
             {
                 Console.WriteLine(string.Format(@"UserId:{0},Name:{1},Email:{2},IsEnable:{3},GuidID:{4},Money:{5},LastUpdateTime:{6}",
-                    item.UserId,item.Name,item.Email,item.IsEnable,item.GuidID,item.Money,item.LastUpdateTime));
+                    item.UserId, item.Name, item.Email, item.IsEnable, item.GuidID, item.Money, item.LastUpdateTime));
             }
         }
 
+        #endregion
 
+        #region 資料交易
+
+        /// <summary>
+        /// Transaction
+        /// </summary>
+        private static void Transaction()
+        {
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("----------Transaction----------");
+
+            int result = 0;
+
+            using (SqlConnection conn = new SqlConnection(DefaultConn))
+            {
+                List<Users> update1 = new List<Users>() {
+                    new Users(){
+                        UserId = 1003,
+                        Email = "ccc@gmail.com"
+                    }
+                };
+
+                List<Users> update2 = new List<Users>(){
+                    new Users(){
+                        UserId = 1004,
+                        Email = "ccc@gmail.com"
+                    }
+                };
+
+                string strSql = @"
+     UPDATE [Users]
+     SET    Email = @Email
+     WHERE  UserId = @UserId ;
+";
+
+                conn.Open();
+
+                //交易
+                using (SqlTransaction tran = conn.BeginTransaction())
+                {
+                    int result1 = conn.Execute(strSql, update1, tran);
+
+                    #region 故意出錯測試 update1 資料是否有修改
+                    //string str = null;
+                    //str.ToString();
+                    #endregion
+
+                    int result2 = conn.Execute(strSql, update2, tran);
+
+                    tran.Commit();
+
+                    result += result1;
+                    result += result2;
+                }
+            }
+
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Format("-----Transaction異動資料{0}筆-----", result));
+        }
+
+        /// <summary>
+        /// TransactionScope
+        /// </summary>
+        private static void TransactionScope()
+        {
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("----------TransactionScope----------");
+
+            int result = 0;
+            int result1 = 0;
+            int result2 = 0;
+          string strSql = @"
+     UPDATE [Users]
+     SET    Email = @Email
+     WHERE  UserId = @UserId ;
+";
+
+            using (TransactionScope scope = new TransactionScope())
+            {
+                using (SqlConnection conn = new SqlConnection(DefaultConn))
+                {
+                    List<Users> update1 = new List<Users>() {
+                    new Users(){
+                        UserId = 1003,
+                        Email = "xxx@gmail.com"
+                    }};
+
+                    List<Users> update2 = new List<Users>(){
+                    new Users(){
+                        UserId = 1004,
+                        Email = "xxx@gmail.com"
+                    }};
+
+                    result1 = conn.Execute(strSql, update1);
+
+                    #region 故意出錯測試 update1 資料是否有修改
+                    //string str = null;
+                    //str.ToString();
+                    #endregion
+
+                    result2 = conn.Execute(strSql, update2);
+                }
+
+                scope.Complete();
+                result += result1;
+                result += result2;
+            }
+
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Format("-----TransactionScope異動資料{0}筆-----", result));
+        }
+
+        #endregion
     }
 
     class Users
